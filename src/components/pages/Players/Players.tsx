882df87482ab05bb0560c12356040b647b04641e
NataliaHuberna <natalyahuberna@gmail.com>
2022-02-14 05:25:35 +0200

    added players page

--- src/components/pages/Players/Players.tsx
+++ src/components/pages/Players/Players.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import axios from 'axios';
+import { BACKEND_URL } from '../../../constants/url';
+import { useEffect, useState } from 'react';
+import { COLORS } from '../../../constants/colors';
+import {StTableDiv} from './styled';
 
-const Players = () => {
+const columns = [
+    { field: 'id', headerName: 'ID', width: 100},
+    { field: 'login', headerName: 'Login', width: 300 },
+    { field: 'email', headerName: 'E-mail', width: 450 },
+    { field: 'games', headerName: 'Games',width: 100 },
+    { field: 'wins', headerName: 'Wins', width: 100 },
+    { field: 'loses', headerName: 'Loses', width: 100 },
+    { field: 'draws', headerName: 'Draws', width: 100 },
+];
+
+const Players = ({ theme }: any) => {
+    const [ tableData, setTableData ] = useState([]);
+    const getPlayers = async () => {
+        try {
+            const { data } = await axios.get(BACKEND_URL.PLAYERS_URL);
+            setTableData(data);
+        } catch (error) {
+            return false;
+        }
+    };
+    useEffect(() => {
+        getPlayers();
+    }, []);
     return (
-        <div>
-            Players
-        </div>
+        <StTableDiv>
+            <DataGrid
+                sx={{
+                    borderColor: `${theme === 'light'
+                        ? `${COLORS.BACKGROUND_HEADER_LIGHT}`
+                        : `${COLORS.SEMI_PRIMARY_COLOR_SEC}`}`,
+                    backgroundColor: `${theme === 'light'
+                        ? `${COLORS.BACKGROUND_MAIN}` : `${COLORS.TABLE_GRID_DARK}`}`,
+                    color: `${theme === 'light' ? `${COLORS.BLACK}` : 'black'}`,
+                    fontSize: '20px',
+                    textAlign: 'center',
+                }}
+                rows={tableData}
+                columns={columns}
+                pageSize={12}
+                rowsPerPageOptions={[ 12 ]}
+            />
+        </StTableDiv>
+
     );
 };
 
